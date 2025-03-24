12b277b8dbb76e465d85252c02f390a4f96b0016
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-03-24 02:40:10 +0100

    added defualt value for min payment for a goal

--- database/migrations/2025_02_27_110906_create_goals_table.php
+++ database/migrations/2025_02_27_110906_create_goals_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->decimal('current_amount', 15, 2)->default(0);
             $table->date('start_date');
             $table->date('target_date');
-            $table->integer('minimum_payment');
+            $table->integer('minimum_payment')->default(0);
             $table->enum('status', ['in_progress', 'achieved', 'abandoned'])->default('in_progress');
             $table->timestamps();
         });
