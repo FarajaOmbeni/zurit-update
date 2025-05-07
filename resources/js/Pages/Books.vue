6e9c32da304163eb8dfeeb6c3e7d131e9c729918
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-05-08 01:54:10 +0300

    added the new e-book

--- resources/js/Pages/Books.vue
+++ resources/js/Pages/Books.vue
@@ -4,7 +4,7 @@
     <Navbar />
     <div class="mt-12 p-16">
         <h2 class="text-3xl mb-4 lg:text-4xl font-bold text-yellow-600">Books</h2>
-        <div class="flex flex-col md:flex-row items-center md:items-start justify-center gap-12">
+        <div class="flex flex-col flex-wrap md:flex-row items-center md:items-start justify-center gap-12">
             <BookCard :book="{
                 title: 'Thrive',
                 description: 'Your ultimate guide to financial freedom. Clear, concise steps to wealth creation.',
@@ -22,6 +22,13 @@
                 description: 'Navigating the Realities - Beyond the allure of Entrepreneurship',
                 price: 1800,
                 image: '/images/books/transcend.jpg'
+            }" />
+
+            <BookCard :book="{
+                title: 'From Earning to Empowerment',
+                description: 'A guide to financial wellness',
+                price: 350,
+                image: '/images/books/earning.png'
             }" />
         </div>
     </div>
