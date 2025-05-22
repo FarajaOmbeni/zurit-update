206fd2f76b6c6aeb3c8eb77a5e232cf79b159523
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-05-22 07:18:28 +0300

    changed zuriscore to read from config

---
+++ config/zuriscore.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ZURIT_USERNAME' => env('ZURIT_USERNAME'),
+    'ZURIT_PASSWORD' => env('ZURIT_PASSWORD'),
+    'ZURIT_URL' => env('ZURIT_URL'),
+    'ZURIT_CALLBACK_URL' => env('ZURIT_CALLBACK_URL'),
+];
