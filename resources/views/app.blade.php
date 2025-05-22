c854f898e450bdd9188dbfa0d8aa88b370283794
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-05-22 17:59:29 +0300

    added manifest.json to app.nlade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
     <link rel="icon" type="image/x-icon" href="/images/home/zurit.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    {{-- Manifest --}}
+    <link rel="manifest" href="/manifest.json">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
