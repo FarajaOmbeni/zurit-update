dfc0dcdedc2216490d3cb8c2e300875f9b173e17
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-06-09 11:14:07 +0300

    modified google analytics code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,19 +24,6 @@
         })(window, document, "clarity", "script", "q821c62vm9");
     </script>
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QZMJCGHRR4"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-QZMJCGHRR4');
-    </script>
-
     {{-- Manifest --}}
     <link rel="manifest" href="/manifest.json">
 
@@ -49,6 +36,18 @@
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QZMJCGHRR4"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-QZMJCGHRR4');
+</script>
 
 <body class="font-sans antialiased">
     @inertia
