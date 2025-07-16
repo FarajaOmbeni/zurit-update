3c0c3d17ca232771b7bc53440a9538741d583ffe
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-07-16 08:06:22 +0300

    updated mmf and stocks

--- resources/js/Components/Variables/investmentTypes.js
+++ resources/js/Components/Variables/investmentTypes.js
@@ -1,103 +1,159 @@
 export const moneyMarketFunds = [
     {
+        value: "cytonn_mmf",
+        label: "Cytonn Money Market Fund",
+        return: "13.40",
+    },
+    {
         value: "gulfcap_mmf",
         label: "Gulfcap Money Market Fund",
-        return: "13.87",
-    },
-    { value: "cytonn_mmf", label: "Cytonn Money Market Fund", return: "13.6" },
-    { value: "kuza_mmf", label: "Kuza Money Market Fund", return: "13.25" },
-    {
-        value: "genafrica_mmf",
-        label: "GenAfrica Money Market Fund",
-        return: "13.1",
+        return: "13.11",
     },
     {
-        value: "arvocap_mmf",
-        label: "Arvocap Money Market Fund",
-        return: "13.07",
-    },
-    { value: "ndovu_mmf", label: "Ndovu Money Market Fund", return: "13.06" },
-    { value: "etica_mmf", label: "Etica Money Market Fund", return: "12.92" },
-    {
-        value: "lofty_corban_mmf",
-        label: "Lofty-Corban Money Market Fund",
-        return: "12.82",
+        value: "ndovu_mmf",
+        label: "Ndovu Money Market Fund",
+        return: "13.06",
     },
     {
-        value: "orient_kasha_mmf",
-        label: "Orient Kasha Money Market Fund",
-        return: "12.79",
+        value: "lotry_corban_mmf",
+        label: "Lotry-Corban Money Market Fund",
+        return: "12.70",
     },
     {
-        value: "enwealth_mmf",
-        label: "Enwealth Money Market Fund",
-        return: "12.08",
+        value: "kuza_mmf",
+        label: "Kuza Money Market Fund",
+        return: "12.57",
     },
     {
-        value: "old_mutual_mmf",
-        label: "Old Mutual Money Market Fund",
-        return: "12.06",
-    },
-    {
-        value: "british_american_mmf",
-        label: "British-American Money Market Fund",
-        return: "11.76",
-    },
-    {
-        value: "jubilee_mmf",
-        label: "Jubilee Money Market Fund",
-        return: "11.49",
-    },
-    {
-        value: "madison_mmf",
-        label: "Madison Money Market Fund",
-        return: "11.43",
+        value: "etica_mmf",
+        label: "Etica Money Market Fund",
+        return: "12.35",
     },
     {
         value: "nabo_africa_mmf",
         label: "Nabo Africa Money Market Fund",
-        return: "11.33",
+        return: "11.85",
+    },
+    {
+        value: "genafrica_mmf",
+        label: "GenAfrica Money Market Fund",
+        return: "11.83",
+    },
+    {
+        value: "enwealth_mmf",
+        label: "Enwealth Money Market Fund",
+        return: "11.04",
+    },
+    {
+        value: "old_mutual_mmf",
+        label: "Old Mutual Money Market Fund",
+        return: "11.02",
+    },
+    {
+        value: "arvocaap_mmf",
+        label: "Arvocaap Money Market Fund",
+        return: "11.02",
+    },
+    {
+        value: "madison_mmf",
+        label: "Madison Money Market Fund",
+        return: "11.00",
+    },
+    {
+        value: "orient_kasha_mmf",
+        label: "Orient Kasha Money Market Fund",
+        return: "10.91",
+    },
+    {
+        value: "britam_mmf",
+        label: "British-American Money Market Fund",
+        return: "10.85",
+    },
+    {
+        value: "jubilee_mmf",
+        label: "Jubilee Money Market Fund",
+        return: "10.78",
+    },
+    {
+        value: "apollo_mmf",
+        label: "Apollo Money Market Fund",
+        return: "10.47",
+    },
+    {
+        value: "faulu_mmf",
+        label: "Faulu Money Market Fund",
+        return: "10.38",
+    },
+    {
+        value: "sanlam_mmf",
+        label: "Sanlam Money Market Fund",
+        return: "10.27",
+    },
+    {
+        value: "dry_associates_mmf",
+        label: "Dry Associates Money Market Fund",
+        return: "10.14",
     },
     {
         value: "genghis_mmf",
         label: "Genghis Money Market Fund",
-        return: "11.21",
+        return: "9.75",
     },
-    { value: "faulu_mmf", label: "Faulu Money Market Fund", return: "11.03" },
     {
-        value: "dry_associates_mmf",
-        label: "Dry Associates Money Market Fund",
-        return: "10.71",
+        value: "kcb_mmf",
+        label: "KCB Money Market Fund",
+        return: "9.71",
     },
-    { value: "sanlam_mmf", label: "Sanlam Money Market Fund", return: "10.64" },
-    { value: "apollo_mmf", label: "Apollo Money Market Fund", return: "10.43" },
-    { value: "kcb_mmf", label: "KCB Money Market Fund", return: "10.23" },
-    { value: "co_op_mmf", label: "Co-op Money Market Fund", return: "10.16" },
-    { value: "cic_mmf", label: "CIC Money Market Fund", return: "10.05" },
     {
-        value: "absa_shilling_mmf",
-        label: "Absa Shilling Money Market Fund",
-        return: "9.93",
+        value: "maai_mmf",
+        label: "Maai Money Market Fund",
+        return: "9.57",
     },
     {
         value: "icea_lion_mmf",
         label: "ICEA Lion Money Market Fund",
-        return: "9.84",
+        return: "9.37",
     },
-    { value: "mali_mmf", label: "Mali Money Market Fund", return: "9.67" },
-    { value: "mayfair_mmf", label: "Mayfair Money Market Fund", return: "9.2" },
     {
-        value: "aa_kenya_shillings_fund",
+        value: "co_op_mmf",
+        label: "Co-op Money Market Fund",
+        return: "8.95",
+    },
+    {
+        value: "cic_mmf",
+        label: "CIC Money Market Fund",
+        return: "8.65",
+    },
+    {
+        value: "mayfair_mmf",
+        label: "Mayfair Money Market Fund",
+        return: "8.60",
+    },
+    {
+        value: "absa_shilling_mmf",
+        label: "Absa Shilling Money Market Fund",
+        return: "8.59",
+    },
+    {
+        value: "cpf_mmf",
+        label: "CPF Money Market Fund",
+        return: "6.94",
+    },
+    {
+        value: "aa_kenya_mmf",
         label: "AA Kenya Shillings Fund",
-        return: "8.06",
+        return: "6.85",
     },
     {
-        value: "stanbic_mmf",
-        label: "Stanbic Money Market Fund",
-        return: "7.46",
+        value: "zidi_mmf",
+        label: "Zidi Money Market Fund",
+        return: "6.69",
     },
-    { value: "zidi_mmf", label: "Zidi Money Market Fund", return: "7.23" },
-    { value: "equity_mmf", label: "Equity Money Market Fund", return: "5.41" },
+    {
+        value: "equity_mmf",
+        label: "Equity Money Market Fund",
+        return: "4.62",
+    },
 ];
 
 export const bonds = [
@@ -168,66 +224,72 @@ export const reits = [
     {
         name: "ILAM FAHARI",
         price: 11,
-    }
-]
+    },
+];
 
 export const shares = [
-  { ticker: "SCOM - Safaricom PLC", price: 23.50 },
-  { ticker: "EQTY - Equity Group Holdings Plc", price: 46.10 },
-  { ticker: "EABL - East African Breweries PLC", price: 186.25 },
-  { ticker: "KCB - KCB Group PLC", price: 44.15 },
-  { ticker: "SCBK - Standard Chartered Bank Kenya Ltd", price: 284.50 },
-  { ticker: "ABSA - Absa Bank Kenya PLC", price: 18.95 },
-  { ticker: "COOP - Co-operative Bank of Kenya Ltd", price: 16.35 },
-  { ticker: "NCBA - NCBA Group PLC", price: 56.00 },
-  { ticker: "SBIC - Stanbic Holdings Plc", price: 160.00 },
-  { ticker: "IMH - I&M Group PLC", price: 34.20 },
-  { ticker: "KEGN - KenGen PLC", price: 5.42 },
-  { ticker: "BAT - British American Tobacco Kenya plc", price: 349.50 },
-  { ticker: "BKG - BK Group PLC", price: 30.55 },
-  { ticker: "KQ - Kenya Airways Plc", price: 4.99 },
-  { ticker: "UMME - Umeme Ltd", price: 12.85 },
-  { ticker: "DTK - Diamond Trust Bank Kenya Ltd", price: 74.00 },
-  { ticker: "BAMB - Bamburi Cement PLC", price: 54.00 },
-  { ticker: "BRIT - Britam Holdings Plc", price: 7.30 },
-  { ticker: "KPLC - Kenya Power & Lighting Co Ltd", price: 8.52 },
-  { ticker: "JUB - Jubilee Holdings Ltd", price: 220.00 },
-  { ticker: "TOTL - TotalEnergies Marketing Kenya PLC", price: 24.50 },
-  { ticker: "HFCK - HF Group PLC", price: 7.08 },
-  { ticker: "KNRE - Kenya Reinsurance Corp. Ltd", price: 1.95 },
-  { ticker: "KUKZ - Kakuzi PLC", price: 400.00 },
-  { ticker: "CTUM - Centum Investment Co Plc", price: 12.00 },
-  { ticker: "CIC - CIC Insurance Group PLC", price: 2.86 },
-  { ticker: "CRWN - Crown Paints Kenya PLC", price: 43.95 },
-  { ticker: "CARB - Carbacid Investments Plc", price: 20.30 },
-  { ticker: "LBTY - Liberty Kenya Holdings Plc", price: 9.50 },
-  { ticker: "TPSE - TPS Eastern Africa Plc", price: 14.20 },
-  { ticker: "WTK - Williamson Tea Kenya Plc", price: 205.75 },
-  { ticker: "SASN - Sasini PLC", price: 14.50 },
-  { ticker: "PORT - East African Portland Cement PLC", price: 36.50 },
-  { ticker: "NBV - Nairobi Business Ventures PLC", price: 1.86 },
-  { ticker: "NSE - Nairobi Securities Exchange PLC", price: 8.00 },
-  { ticker: "NMG - Nation Media Group PLC", price: 12.00 },
-  { ticker: "KAPC - Kapchorua Tea Kenya PLC", price: 220.00 },
-  { ticker: "BOC - BOC Kenya PLC", price: 85.00 },
-  { ticker: "CGEN - Car & General (Kenya) Plc", price: 20.35 },
-  { ticker: "UNGA - Unga Group Plc", price: 20.65 },
-  { ticker: "TCL - TransCentury PLC", price: 1.17 },
-  { ticker: "SCAN - WPP Scangroup Plc", price: 2.50 },
-  { ticker: "SMER - Sameer Africa PLC", price: 3.61 },
-  { ticker: "SLAM - Sanlam Kenya Plc", price: 6.88 },
-  { ticker: "LKL - Longhorn Publishers PLC", price: 2.83 },
-  { ticker: "LIMT - Limuru Tea PLC", price: 320.00 },
-  { ticker: "AMAC - Africa Mega Agricorp PLC", price: 56.00 },
-  { ticker: "SGL - Standard Group PLC", price: 6.08 },
-  { ticker: "CABL - East African Cables PLC", price: 1.88 },
-  { ticker: "EGAD - Eaagads Ltd", price: 12.40 },
-  { ticker: "HAFR - Home Afrika Ltd", price: 0.70 },
-  { ticker: "FTGH - FTG Holdings Ltd", price: 1.21 },
-  { ticker: "EVRD - Eveready East Africa PLC", price: 0.85 },
-  { ticker: "XPRS - Express Kenya PLC", price: 3.14 },
-  { ticker: "OCH - Olympia Capital Holdings Ltd", price: 3.70 },
-  { ticker: "UCHM - Uchumi Supermarkets Ltd", price: 0.28 }
+    {
+        ticker: "SCOM - Safaricom PLC",
+        price: 23.5,
+        dividend:
+            "Final Dividend of Kes. 0.65, announced on 09-May-2025, Books Closure: 31-Jul-2025, Payment Date: 31-Aug-2025",
+    },
+    {
+        ticker: "UMME - Umeme Ltd",
+        price: 12.85,
+        dividend:
+            "Interim Dividend of Ush 222, announced on 24-Jun-2025, Books Closure: 14-Jul-2025, Payment Date: 31-Jul-2025",
+    },
+    {
+        ticker: "JUB - Jubilee Holdings Ltd",
+        price: 220.0,
+        dividend:
+            "Final Dividend of Kes. 11.50, announced on 04-Apr-2025, Books Closure: 28-May-2025, Payment Date: 25-Jul-2025",
+    },
+    {
+        ticker: "TOTL - TotalEnergies Marketing Kenya PLC",
+        price: 24.5,
+        dividend:
+            "First & Final Dividend of Kes. 1.92, announced on 30-Apr-2025, Books Closure: 27-Jun-2025, Payment Date: 31-Jul-2025",
+    },
+    {
+        ticker: "KNRE - Kenya Reinsurance Corp. Ltd",
+        price: 1.95,
+        dividend:
+            "Final Dividend of Kes. 0.15, announced on 28-Mar-2025, Books Closure: 20-Jun-2025, Payment Date: 01-Aug-2025",
+    },
+    {
+        ticker: "LBTY - Liberty Kenya Holdings Plc",
+        price: 9.5,
+        dividend:
+            "Final Dividend of Kes. 1.00, Special Dividend of Kes. 0.60, announced on 26-Mar-2025, Books Closure: 14-Jun-2025, Payment Date: 24-Aug-2025",
+    },
+    {
+        ticker: "TPSE - TPS Eastern Africa Plc",
+        price: 14.2,
+        dividend:
+            "First & Final Dividend of Kes. 0.35, announced on 14-May-2025, Books Closure: 26-Jun-2025, Payment Date: 30-Jul-2025",
+    },
+    {
+        ticker: "WTK - Williamson Tea Kenya Plc",
+        price: 205.75,
+        dividend:
+            "First & Final Dividend of Kes. 10.00, announced on 27-Jun-2025, Books Closure: 31-Jul-2025, Payment Date: 02-Sep-2025",
+        bonus: "Ratio of 1:1, announced on 27-Jun-2025, Books Closure: Subject to Approval, Payment Date: Subject to Approval",
+    },
+    {
+        ticker: "NSE - Nairobi Securities Exchange PLC",
+        price: 8.0,
+        dividend:
+            "First & Final Dividend of Kes. 0.32, announced on 28-Mar-2025, Books Closure: 21-May-2025, Payment Date: 31-Jul-2025",
+    },
+    {
+        ticker: "KAPC - Kapchorua Tea Kenya PLC",
+        price: 220.0,
+        dividend:
+            "First & Final Dividend of Kes. 25.00, announced on 27-Jun-2025, Books Closure: 31-Jul-2025, Payment Date: 02-Sep-2025",
+        bonus: "Ratio of 1:1, announced on 27-Jun-2025, Books Closure: Subject to Approval, Payment Date: Subject to Approval",
+    },
 ];
 
 export const insurances = [
