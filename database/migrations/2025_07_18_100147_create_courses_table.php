af51762270e245cec55e68a46a6f58b76f08f33d
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-08-08 23:27:13 +0300

    modified the migrations

--- database/migrations/2025_07_18_100147_create_courses_table.php
+++ database/migrations/2025_07_18_100147_create_courses_table.php
@@ -10,7 +10,10 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('courses')->onDelete('cascade');
             $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('order')->default(0);
             $table->string('slug')->unique();
             $table->timestamps();
         });
