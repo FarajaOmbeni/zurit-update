bd1f13fe2d81474ece6b074b81f093f656ed42f1
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-09-22 15:34:36 +0300

    updated the video

--- public/build/assets/AuthenticatedLayout-BRbG05M7.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{b as a,o as s,a as e,E as r}from"./app-Zm4Con3X.js";const n={},c={class:"bg-gray-100"};function l(t,_){return s(),a("div",null,[e("div",c,[e("main",null,[r(t.$slots,"default")])])])}const f=o(n,[["render",l]]);export{f as A};
