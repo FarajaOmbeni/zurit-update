cacbd11f7a6e1194b6b04c5c61468cbe752fb2ee
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-09-24 10:57:18 +0300

    deploy files

--- public/build/assets/useAlert-0BpybJyC.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./app-C2E92Tfv.js";const e=o(null);function s(){return{alertState:e,openAlert:(r,t,l=5e3,n=!0)=>{e.value={type:r,message:t,duration:l,autoClose:n}},clearAlert:()=>{e.value=null}}}export{s as u};
