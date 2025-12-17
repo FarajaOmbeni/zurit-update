49b4b21c147d1d708fad866f9fbc56610277c1ba
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-12-17 10:09:03 +0300

    updated investments

--- public/build/assets/useAlert-BHLczwKh.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./app-CJE-g-JS.js";const e=o(null);function s(){return{alertState:e,openAlert:(r,t,l=5e3,n=!0)=>{e.value={type:r,message:t,duration:l,autoClose:n}},clearAlert:()=>{e.value=null}}}export{s as u};
